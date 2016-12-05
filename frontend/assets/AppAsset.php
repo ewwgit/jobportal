5035cb9e4b6103c76dd51bef57d413b5a483b450
komali <support@EWW22.eww.local>
2016-12-05 17:23:35 +0530

    commit the upadated appasset

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,12 +13,54 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         //'css/site.css',
-    		'css/style.css',
-    		'css/bootstrap-3.1.1.min.css',
+    		'css/base.css',
+    		'css/bootstrap.min.css',
     		'css/font-awesome.css',
-    		
+    		'css/responsive.css',
+    		'css/style.css',
     ];
-    public $js = [
+    public $script = [
+    		
+    		'script/bootstrap.min.js',
+    			'script/chosen.jquery.min.js',
+    		'script/custom.js',
+    		'script/headroom.min.js',
+    		'script/jquery-2.1.3.min.js',
+    		'script/jquery.counterup.min.js',
+    		'script/jquery.flexslider-min.js',
+    		'script/jquery.gmaps.min.js',
+    		'script/jpanelmenu.js',
+    		'script/jquery.magnific-popup.min.js',
+    		'script/jquery.sceditor.bbcode.min.js',
+    		'script/jquery.sceditor.js',
+    		'script/jquery.superfish.js',
+    		'script/jquery.themepunch.revolution.min.js',
+    		'script/jquery.themepunch.showbizpro.min.js',
+    		'script/jquery.themepunch.tools.min.js',
+    		'script/stacktable.js',
+    		'script/switcher.js',
+    		'script/vendor-date.js',
+    		'script/vendor-datepicker.js',
+    		'script/Waypoints.min.js',
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
     ];
     public $depends = [
         'yii\web\YiiAsset',
