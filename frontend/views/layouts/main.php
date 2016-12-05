7657b7dee801c23c922abe1f29c7394ee2b62dde
komali <support@EWW22.eww.local>
2016-12-05 17:59:26 +0530

    commit the updated main layout page

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -22,217 +22,247 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+ 
 </head>
 <body>
 <?php $this->beginBody() ?>
 
-<nav class="navbar navbar-default" role="navigation">
-	<div class="container">
-	    <div class="navbar-header">
-	        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-	        </button>
-	        <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""/></a>
-	    </div>
-	    <!--/.navbar-header-->
-	    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
-	    
-	        <ul class="nav navbar-nav">
-	           <?php  if (Yii::$app->user->isGuest) { ?> 
-			<li><a href="<?php echo Yii::$app->getHomeUrl(); ?>">Home</a></li>
-	        
-		        <li class="dropdown">
-		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jobs<b class="caret"></b></a>
-		            <ul class="dropdown-menu">
-			            <li><a href="location.html">Contract Jobs</a></li>
-			            <li><a href="location.html">Walkin Jobs</a></li>
-			            <li><a href="location.html">Jobs by Location</a></li>
-			            <li><a href="location.html">Jobs by Function</a></li>
-			            <li><a href="location.html">Jobs by Industry</a></li>
-			            <li><a href="location.html">Jobs by Company</a></li>
-		            </ul>
-		        </li>
-		        <li class="dropdown">
-		        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
-		            <ul class="dropdown-menu multi-column columns-3">
-			            <div class="row">
-				            <div class="col-sm-4">
-					            <ul class="multi-column-dropdown">
-						            <li><a href="services.html">Action</a></li>
-						            <li><a href="services.html">Another action</a></li>
-						            <li><a href="services.html">Something else here</a></li>
-						            <li class="divider"></li>
-						            <li><a href="services.html">Separated link</a></li>
-						            <li class="divider"></li>
-						            <li><a href="services.html">One more separated link</a></li>
-					            </ul>
-				            </div>
-				            <div class="col-sm-4">
-					            <ul class="multi-column-dropdown">
-						            <li><a href="services.html">Action</a></li>
-						            <li><a href="services.html">Another action</a></li>
-						            <li><a href="services.html">Something else here</a></li>
-						            <li class="divider"></li>
-						            <li><a href="services.html">Separated link</a></li>
-						            <li class="divider"></li>
-						            <li><a href="services.html">One more separated link</a></li>
-					            </ul>
-				            </div>
-				            <div class="col-sm-4">
-					            <ul class="multi-column-dropdown">
-						            <li><a href="services.html">Action</a></li>
-						            <li><a href="services.html">Another action</a></li>
-						            <li><a href="services.html">Something else here</a></li>
-						            <li class="divider"></li>
-						            <li><a href="services.html">Separated link</a></li>
-						            <li class="divider"></li>
-						            <li><a href="services.html">One more separated link</a></li>
-					            </ul>
-				            </div>
-			            </div>
-		            </ul>
-		        </li>
-		        <li class="dropdown">
-		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Recruiters<b class="caret"></b></a>
-		             <ul class="dropdown-menu">
-			            <li><a href="login.html">Recruiter Updates</a></li>
-			            <li><a href="recruiters.html">Recruiters you are following</a></li>
-			            <li><a href="codes.html">Shortcodes</a></li>
-		             </ul>
-		        </li>
-		        <li class="dropdown">
-		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">More<b class="caret"></b></a>
-		            <ul class="dropdown-menu">
-			            <li><a href="jobs.html">Walk-ins</a></li>
-			            <li><a href="jobs.html">Bpo Jobs</a></li>
-			            <li><a href="jobs.html">Teaching Jobs</a></li>
-			            <li><a href="jobs.html">Diploma Jobs</a></li>
-			            <li><a href="jobs.html">Tech Support</a></li>
-			            <li><a href="jobs.html">Finance Jobs</a></li>
-			            <li><a href="jobs.html">Part time Jobs</a></li>
-			            <li><a href="jobs.html">Health Care</a></li>
-			            <li><a href="jobs.html">Hospitality</a></li>
-			            <li><a href="jobs.html">Internships</a></li>
-			            <li><a href="jobs.html">Research Jobs</a></li>
-			            <li><a href="jobs.html">Defence Jobs</a></li>
-		            </ul>
-		        </li>
-		       <li><a href="<?= Url::to(['/site/login'])?>" title="login">Employee-login</a></li>
-		        <!-- <li><a href="resume.html">Upload Resume</a></li> -->
-		         <li><a href="<?= Url::to(['/site/signup'])?>" title="signup">Employee-Signup</a></li>
-		         <li><a href="<?= Url::to(['/employercompany/empsite/signup'])?>" title="viewprofile">EMPLOYER</a></li>
-	        </ul>
-	        <?php }else { ?>
-	        <ul class="nav navbar-nav">
-	        <li><a href="<?= Url::to(['/common/employee'])?>" title="viewprofile">EmployeeProfile</a></li>
-	         <li><a href="<?= Url::to(['site/viewprofile'])?>" title="viewprofile">EmployeeView</a></li>
-	         <li><a href="<?= Url::to(['/employercompany/empcommon/jobpostingslist'])?>" title="viewprofile">EmployerJobPosting</a></li>
-			    <li><a href="<?= Url::to(['/employercompany/empcommon/employer'])?>" title="viewprofile">EmployerDetails</a></li>
-               <li><a href="<?= Url::to(['/site/logout'])?>"
-            data-method="post" title="">Logout</a></li>
-            </ul>
-            <?php } ?>
-			
-	    </div>
-	    <div class="clearfix"> </div>
-	  </div>
-	    <!--/.navbar-collapse-->
-	</nav>
+ <header class="transparent sticky-header full-width">
+    <div class="container">
+      <div class="sixteen columns"> 
+        
+        <!-- Logo -->
+        <div id="logo">
+          <h1><a href="index.html"><img src="images/logo2.png" alt="Work Scout" /></a></h1>
+        </div>
+        
+        <!-- Menu -->
+        <nav id="navigation" class="menu">
+          <ul id="responsive">
+            <li><a href="<?php echo Yii::$app->getHomeUrl(); ?>">Home</a> </li>
+            <li><a href="#">About Us</a> </li>
+            
+            <!--<li><a href="#">Pages</a>
+					<ul>
+						<li><a href="job-page.html">Job Page</a></li>
+						<li><a href="job-page-alt.html">Job Page Alternative</a></li>
+						<li><a href="resume-page.html">Resume Page</a></li>
+						<li><a href="shortcodes.html">Shortcodes</a></li>
+						<li><a href="icons.html">Icons</a></li>
+						<li><a href="pricing-tables.html">Pricing Tables</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</li>
+-->
+            <li><a href="#">Candidates</a>
+              <ul>
+                <li><a href="browse-jobs.html">Browse Jobs</a></li>
+                <li><a href="browse-categories.html">Browse Categories</a></li>
+                <li><a href="add-resume.html">Add Resume</a></li>
+                <li><a href="manage-resumes.html">Manage Resumes</a></li>
+                <li><a href="job-alerts.html">Job Alerts</a></li>
+              </ul>
+            </li>
+            
+            <!--<li><a href="#">For Employers</a>
+					<ul>
+						<li><a href="add-job.html">Add Job</a></li>
+						<li><a href="manage-jobs.html">Manage Jobs</a></li>
+						<li><a href="manage-applications.html">Manage Applications</a></li>
+						<li><a href="browse-resumes.html">Browse Resumes</a></li>
+					</ul>
+				</li>-->
+            
+          </ul>
+          <ul class="float-right">
+            <li><a href="<?= Url::to(['/site/signup'])?>" title="signup"><i class="fa fa-user"></i> Sign Up</a></li>
+            <li><a href="Candidates-login.html"><i class="fa fa-lock"></i> Log In</a></li>
+            <li><a href="empolyer.html"><i class="fa fa-user"></i>Employer Zone</a></li>
+          </ul>
+        </nav>
+        
+        <!-- Navigation -->
+        <div id="mobile-navigation"> <a href="#menu" class="menu-trigger"><i class="fa fa-reorder"></i> Menu</a> </div>
+      </div>
+    </div>
+  </header>
+   <div id="banner" class="with-transparent-header parallax background" style="background-image: url(images/banner-home-02.jpg)" data-img-width="2000"     data-img-height="1330" data-diff="300">
+    <div class="container" >
+      <div class="sixteen columns">
+        <div class="search-container"> 
+          
+          <!-- Form -->
+          <h2>Find job</h2>
+          <input type="text" class="ico-01" placeholder="job title, keywords or company name" value=""/>
+          <input type="text" class="ico-02" placeholder="city, province or region" value=""/>
+          <button><i class="fa fa-search"></i></button>
+          
+          <!-- Browse Jobs -->
+          <div class="browse-jobs"> Browse job offers by <a href="browse-categories.html"> category</a> or <a href="#">location</a> </div>
+          
+          <!-- Announce -->
+          <div class="announce"> We’ve over <strong>15 000</strong> job offers for you! </div>
+        </div>
+      </div>
+    </div>
+  </div>
 	
-	<div class="container">
+	<div class="container" style="width: 100%;">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
-      <div class="container">
-    <footer class="footer">
-  
-      
-<div class="footer">
-	<div class="container">
-		<div class="col-md-3 grid_3">
-			<h4>Navigate</h4>
-			<ul class="f_list f_list1">
-				<li><a href="index.html">Home</a></li>
-				<li><a href="login.html">Sign In</a></li>
-				<li><a href="login.html">Join Now</a></li>
-				<li><a href="about.html">About</a></li>
-			</ul>
-			<ul class="f_list">
-				<li><a href="features.html">Features</a></li>
-				<li><a href="terms.html">Terms of use</a></li>
-				<li><a href="contact.html">Contact Us</a></li>
-				<li><a href="jobs.html">Post a Job</a></li>
-			</ul>
-			<div class="clearfix"> </div>
-		</div>
-		<div class="col-md-3 grid_3">
-			<h4>Twitter Widget</h4>
-			<div class="footer-list">
-			  <ul>
-				<li><i class="fa fa-twitter tw1"> </i><p><span class="yellow"><a href="#">consectetuer</a></span> adipiscing elit web design</p></li>
-				<li><i class="fa fa-twitter tw1"> </i><p><span class="yellow"><a href="#">consectetuer</a></span> adipiscing elit web design</p></li>
-				<li><i class="fa fa-twitter tw1"> </i><p><span class="yellow"><a href="#">consectetuer</a></span> adipiscing elit web design</p></li>
-			  </ul>
-			</div>
-		</div>
-		<div class="col-md-3 grid_3">
-			<h4>Seeking</h4>
-			<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-		</div>
-		<div class="col-md-3 grid_3">
-			<h4>Sign up for our newsletter</h4>
-			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.</p>
-			<form>
-				<input type="text" class="form-control" placeholder="Enter your email">
-				<button type="button" class="btn red">Subscribe now!</button>
-		    </form>
-		</div>
-		<div class="clearfix"> </div>
-	</div>
-</div>
-<div class="footer_bottom">	
-  <div class="container">
-    <div class="col-sm-2">
-  		<ul class="f_list2">
-			<li><a href="jobs.html">Russia Jobs</a></li>
-			<li><a href="jobs.html">Australia Jobs</a></li>
-			<li><a href="jobs.html">Srilanka Jobs</a></li>
-			<li><a href="jobs.html">Poland Jobs</a></li>
-	    </ul>
-  	</div>
-  	<div class="col-sm-2">
-  		<ul class="f_list2">
-			<li><a href="jobs.html">Newzland Jobs</a></li>
-			<li><a href="jobs.html">Pakistan Jobs</a></li>
-			<li><a href="jobs.html">Srilanka Jobs</a></li>
-			<li><a href="jobs.html">Irland Jobs</a></li>
-	    </ul>
-  	</div>
-  	<div class="col-sm-2">
-  		<ul class="f_list2">
-			<li><a href="jobs.html">Canada Jobs</a></li>
-			<li><a href="jobs.html">Germany Jobs</a></li>
-			<li><a href="jobs.html">China Jobs</a></li>
-			<li><a href="jobs.html">Nepal Jobs</a></li>
-	    </ul>
-  	</div>
-  	<div class="col-sm-6 footer_text">
-       <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numqua"</p>
-  	</div>
-  	<div class="clearfix"> </div>
-	<div class="copy">
-		<p>Copyright © 2015 Seeking . All Rights Reserved . Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
-	</div>
-  </div>
-</div>
-</footer>
+    
+<div class="margin-top-15"></div>
+  <div id="footer"> 
+    <!-- Main -->
+    <div class="container">
+      <div class="seven columns">
+        <h4>About</h4>
+        <p>Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper.</p>
+        <a href="#" class="button">Get Started</a> </div>
+      <div class="three columns">
+        <h4>Company</h4>
+        <ul class="footer-links">
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Careers</a></li>
+          <li><a href="#">Our Blog</a></li>
+          <li><a href="#">Terms of Service</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Hiring Hub</a></li>
+        </ul>
+      </div>
+      <div class="three columns">
+        <h4>Press</h4>
+        <ul class="footer-links">
+          <li><a href="#">In the News</a></li>
+          <li><a href="#">Press Releases</a></li>
+          <li><a href="#">Awards</a></li>
+          <li><a href="#">Testimonials</a></li>
+          <li><a href="#">Timeline</a></li>
+        </ul>
+      </div>
+      <div class="three columns">
+        <h4>Browse</h4>
+        <ul class="footer-links">
+          <li><a href="#">Freelancers by Category</a></li>
+          <li><a href="#">Freelancers in USA</a></li>
+          <li><a href="#">Freelancers in UK</a></li>
+          <li><a href="#">Freelancers in Canada</a></li>
+          <li><a href="#">Freelancers in Australia</a></li>
+          <li><a href="#">Find Jobs</a></li>
+        </ul>
+      </div>
     </div>
+    
+    <!-- Bottom -->
+    <div class="container">
+      <div class="footer-bottom">
+        <div class="sixteen columns">
+          <h4>Follow Us</h4>
+          <ul class="social-icons">
+            <li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+            <li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+            <li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+            <li><a class="linkedin" href="#"><i class="icon-linkedin"></i></a></li>
+          </ul>
+          <div class="copyrights">©  Copyright 2016 by <a href="#">Anaghasoftech</a>. All Rights Reserved.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Back To Top Button -->
+  <div id="backtotop"><a href="#"></a></div>
+</div>
+<!-- Wrapper / End --> 
+
+<!-- Scripts-->
+
+<!-- Style Switcher
+================================================== --> 
+<script src="scripts/switcher.js"></script>
+<div id="style-switcher">
+  <h2>Style Switcher <a href="#"></a></h2>
+  <div>
+    <h3>Predefined Colors</h3>
+    <ul class="colors" id="color1">
+      <li><a href="#" class="green" title="Green"></a></li>
+      <li><a href="#" class="blue" title="Blue"></a></li>
+      <li><a href="#" class="orange" title="Orange"></a></li>
+      <li><a href="#" class="navy" title="Navy"></a></li>
+      <li><a href="#" class="yellow" title="Yellow"></a></li>
+      <li><a href="#" class="peach" title="Peach"></a></li>
+      <li><a href="#" class="beige" title="Beige"></a></li>
+      <li><a href="#" class="purple" title="Purple"></a></li>
+      <li><a href="#" class="celadon" title="Celadon"></a></li>
+      <li><a href="#" class="pink" title="Pink"></a></li>
+      <li><a href="#" class="red" title="Red"></a></li>
+      <li><a href="#" class="brown" title="Brown"></a></li>
+      <li><a href="#" class="cherry" title="Cherry"></a></li>
+      <li><a href="#" class="cyan" title="Cyan"></a></li>
+      <li><a href="#" class="gray" title="Gray"></a></li>
+      <li><a href="#" class="olive" title="Olive"></a></li>
+    </ul>
+    <h3>Layout Style</h3>
+    <div class="layout-style">
+      <select id="layout-style">
+        <option value="2">Wide</option>
+        <option value="1">Boxed</option>
+      </select>
+    </div>
+    <h3>Header Style</h3>
+    <div class="layout-style">
+      <select id="header-style">
+        <option value="1">Style 1</option>
+        <option value="2">Style 2</option>
+        <option value="3">Style 3</option>
+      </select>
+    </div>
+    <h3>Background Image</h3>
+    <ul class="colors bg" id="bg">
+      <li><a href="#" class="bg1"></a></li>
+      <li><a href="#" class="bg2"></a></li>
+      <li><a href="#" class="bg3"></a></li>
+      <li><a href="#" class="bg4"></a></li>
+      <li><a href="#" class="bg5"></a></li>
+      <li><a href="#" class="bg6"></a></li>
+      <li><a href="#" class="bg7"></a></li>
+      <li><a href="#" class="bg8"></a></li>
+      <li><a href="#" class="bg9"></a></li>
+      <li><a href="#" class="bg10"></a></li>
+      <li><a href="#" class="bg11"></a></li>
+      <li><a href="#" class="bg12"></a></li>
+      <li><a href="#" class="bg13"></a></li>
+      <li><a href="#" class="bg14"></a></li>
+      <li><a href="#" class="bg15"></a></li>
+      <li><a href="#" class="bg16"></a></li>
+    </ul>
+    <h3>Background Color</h3>
+    <ul class="colors bgsolid" id="bgsolid">
+      <li><a href="#" class="green-bg" title="Green"></a></li>
+      <li><a href="#" class="blue-bg" title="Blue"></a></li>
+      <li><a href="#" class="orange-bg" title="Orange"></a></li>
+      <li><a href="#" class="navy-bg" title="Navy"></a></li>
+      <li><a href="#" class="yellow-bg" title="Yellow"></a></li>
+      <li><a href="#" class="peach-bg" title="Peach"></a></li>
+      <li><a href="#" class="beige-bg" title="Beige"></a></li>
+      <li><a href="#" class="purple-bg" title="Purple"></a></li>
+      <li><a href="#" class="red-bg" title="Red"></a></li>
+      <li><a href="#" class="pink-bg" title="Pink"></a></li>
+      <li><a href="#" class="celadon-bg" title="Celadon"></a></li>
+      <li><a href="#" class="brown-bg" title="Brown"></a></li>
+      <li><a href="#" class="cherry-bg" title="Cherry"></a></li>
+      <li><a href="#" class="cyan-bg" title="Cyan"></a></li>
+      <li><a href="#" class="gray-bg" title="Gray"></a></li>
+      <li><a href="#" class="olive-bg" title="Olive"></a></li>
+    </ul>
+  </div>
+  <div id="reset"><a href="#" class="button color">Reset</a></div>
+</div>
+
 <?php $this->endBody() ?>
 </body>
 </html>
