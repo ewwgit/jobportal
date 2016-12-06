e1ed82c322033aced64bea2ffa449ac40fc5725b
komali <support@EWW22.eww.local>
2016-12-06 16:10:51 +0530

    commit the indexpage after banner

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -38,11 +38,31 @@ $this->title = 'My Yii Application';
   <!-- Header
 ================================================== -->
  
-  <div class="clearfix"></div>
+ 
   
   <!-- Banner
 ================================================== -->
- 
+  <div id="banner" class="with-transparent-header parallax background" style="background-image: url(images/banner-home-02.jpg)" data-img-width="2000"     data-img-height="1330" data-diff="300">
+    <div class="container" >
+      <div class="sixteen columns">
+        <div class="search-container"> 
+          
+          <!-- Form -->
+          <h2>Find job</h2>
+          <input type="text" class="ico-01" placeholder="job title, keywords or company name" value=""/>
+          <input type="text" class="ico-02" placeholder="city, province or region" value=""/>
+          <button><i class="fa fa-search"></i></button>
+          
+          <!-- Browse Jobs -->
+          <div class="browse-jobs"> Browse job offers by <a href="browse-categories.html"> category</a> or <a href="#">location</a> </div>
+          
+          <!-- Announce -->
+          <div class="announce"> We’ve over <strong>15 000</strong> job offers for you! </div>
+        </div>
+      </div>
+    </div>
+  </div>
+	
   
   <!-- Content
 ================================================== --> 
