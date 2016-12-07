8f0c516518b7021785a88c55ec4e23d0e5da77a1
santosh <santosh@EWW18.eww.local>
2016-12-07 18:25:15 +0530

    Updated with latest updations

--- frontend/modules/employercompany/views/empsite/signup.php
+++ frontend/modules/employercompany/views/empsite/signup.php
@@ -8,83 +8,97 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\frontend\models\SignupForm;
 use yii\helpers\Url;
+use kartik\date\DatePicker;
 
 $this->title = 'Signup';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
-
+<style>
+select {
+	height: 40px !important;
+}
+</style>
  <div class="my-account">
 <div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to signup:</p>
-
+    <ul class="tabs-nav">
+			<li class="in-active"><a href="<?= Url::to(['/site/login'])?>" title="login">Login</a></li>
+			<li class="active"><a href="<?= Url::to(['/employercompany/empsite/signup'])?>" title="signup">Register</a></li>
+		</ul>
      <div class="tabs-container"> 
        <div class="tab-content" id="tab1" >
 
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
             
             	 <p class="form-row form-row-wide">
-              <label for="username">name: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'name')->textInput(['autofocus' => true])->label(false) ?>
+              <label for="username">Name: 
+              <?= $form->field($model, 'name')->textInput(['autofocus' => true, 'placeholder' => 'Name', 'class' => 'fa fa-user'])->label(false) ?>
               </label>
             </p>
             
 
              	 <p class="form-row form-row-wide">
-              <label for="username">Username: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label(false) ?>
+              <label for="username">Username: 
+              <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Username',])->label(false) ?>
               </label>
             </p>
 
              	 <p class="form-row form-row-wide">
-              <label for="username">Mobilenumber: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'mobilenumber')->textInput(['autofocus' => true])->label(false) ?>
+              <label for="username">Mobilenumber:
+              <?= $form->field($model, 'mobilenumber')->textInput(['autofocus' => true,'placeholder' => 'Mobilenumber',])->label(false) ?>
               </label>
             </p>
                 
                <p class="form-row form-row-wide">
-              <label for="username">Email: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'email')->textInput(['autofocus' => true])->label(false) ?>
+              <label for="username">Email: 
+              <?= $form->field($model, 'email')->textInput(['autofocus' => true,'placeholder' => 'Email',])->label(false) ?>
               </label>
             </p>
 
             <p class="form-row form-row-wide">
-              <label for="username">Password: <i class="fa fa-user"></i>
+              <label for="username">Password: 
               <?= $form->field($model, 'password')->passwordInput() ->label(false) ?>
               </label>
             </p>
 
             <p class="form-row form-row-wide">
-              <label for="username">Dateofbirth: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'dateofbirth')->textInput(['type' => 'date']) ->label(false) ?>
+              <label for="username">Dateofbirth:
+              <?=$form->field ( $model, 'dateofbirth' )->widget ( DatePicker::classname (), [ 'options' => [ 'placeholder' => 'Enter birth date ...' ],'pluginOptions' => [ 'autoclose' => true ] ] )->label(false);?>
               </label>
             </p>
                 
                <p class="form-row form-row-wide">
-              <label for="username">Gender: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'gender')->radioList(['male' =>'male' , 'female' =>'female'],['prompt' =>'<---select gender--->'])->label(false) ?>
+              <label for="username">Gender:
+              <?= $form->field($model, 'gender')->inline()->radioList(['male' =>'Male' , 'female' =>'Female'],['prompt' =>'<---select gender--->'])->label(false) ?>
               </label>
             </p>
                 
               <p class="form-row form-row-wide">
-              <label for="username">Designation: <i class="fa fa-user"></i>
+              <label for="username">Designation:
               <?= $form->field($model, 'designation')->dropDownList(['Java Developer' =>'Java Developer' , 'Tester' =>'Tester', 'Designer' =>'Designer' , 'Administrator' =>'Administrator' , 'Software Engineer' =>'Software Engineer' , 'Web Developer' =>'Web Developer' , 'Php Developer' =>'Php Developer'],['prompt' =>'select designation'])->label(false) ?>                 
        
               </label>
             </p>
                 
                 <p class="form-row form-row-wide">
-              <label for="username">Address: <i class="fa fa-user"></i>
-              <?= $form->field($model, 'address')->textarea(['rows' => 6])->label(false) ?>
+              <label for="username">Address: 
+              <?= $form->field($model, 'address')->textarea(['rows' => 4])->label(false) ?>
        
               </label>
             </p>
-             
-                <div class="form-group">
-                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
-                     <a href="<?= Url::to(['/employercompany/empsite/login'])?>" title="viewprofile">login</a>
-                </div>
+             <p class="form-row form-row-wide">
+           <div class="form">
+			 <label for="password2">Profileimage</label>
+				<label class="upload-btn"><i class="fa fa-upload"></i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				    <input type="file" multiple />
+				     Browse
+				</label>
+				<span class="fake-input">No file selected</span>
+			</div>
+          
+          </p>
+          <p class="form-row">
+          <?= Html::submitButton('Signup', ['class' => 'button border fw margin-top-10', 'name' => 'register']) ?>
+          </p>
                
 
             <?php ActiveForm::end(); ?>
