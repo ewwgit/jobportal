bf414ca14eb65b9344ca1e1066fab6e5962012fd
expert <expert@EWW28>
2016-12-08 12:07:36 +0530

    jobpostings   search commited

--- frontend/models/JobpostSearch.php
+++ frontend/models/JobpostSearch.php
@@ -12,7 +12,7 @@ class JobpostSearch extends EmployerJobpostings
     public function rules()
     {
         return [
-            [['rolecategory','skills','designation','experience','Description','jobtype','gender','address','company_name','company_type','industry_type','dateofestablishment','country','state','city','zipcode'], 'safe'],
+            [['rolecategory','skills','designation','Min_Experience','Max_Experience','Description','jobtype','gender','address','company_name','company_type','industry_type','dateofestablishment','country','state','city','zipcode'], 'safe'],
        
         ];
     }
@@ -62,7 +62,8 @@ class JobpostSearch extends EmployerJobpostings
         $query->andFilterWhere(['like', 'rolecategory', $this->rolecategory])
             ->andFilterWhere(['like', 'skills', $this->skills])
             ->andFilterWhere(['like', 'designation', $this->designation])
-            ->andFilterWhere(['like', 'experience', $this->experience])
+            ->andFilterWhere(['like', 'Min_Experience', $this->Min_Experience])
+            ->andFilterWhere(['like', 'Max_Experience', $this->Max_Experience])
             ->andFilterWhere(['like', 'dateofestablishment', $this->dateofestablishment]);
 
         return $dataProvider;
