1d51e4853be008208671750abcab8ac2f0920697
expert <expert@EWW28>
2016-12-21 18:56:38 +0530

    commited for  skills searching

--- frontend/modules/employercompany/controllers/EmpcommonController.php
+++ frontend/modules/employercompany/controllers/EmpcommonController.php
@@ -445,9 +445,38 @@ class EmpcommonController extends Controller {
 	
 		$searchModel = new JobpostSearch();
 		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-
+		//$model = new EmployerJobpostings();
+		
+		
+		$rows = (new \yii\db\Query())
+		->select([ 'skills'])
+		->from(' employer_post_jobs')
+		->where('id')->all();
 	
+		
+		// $rows[] = array();
+		
+		$data=var_dump($rows);
+		// print_r($data);exit;
+		
+		
+// 				$skillsdata = EmployerJobpostings::find()
+// 				->select('skills')
+// 				->where(['id' => Yii::$app->employer->employerid])->all();
+		
+		 		 			while($data !== null) {
+		 					$dataaa=explode(',', $data);
+		 				 	foreach($dataaa as $output) {
+		 						//echo $output;
+		 						$searchModel->skills=$output;
+		 						print_r($searchModel->skills);
+		 					}
+		 				}
+		
+                  
+
+	   // $searchModel->skills=$output;
+	    
 		return $this->render('jobgrid', [
 				'dataProvider' => $dataProvider,
 				'searchModel' => $searchModel
