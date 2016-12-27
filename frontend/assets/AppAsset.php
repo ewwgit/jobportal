fbb2933e8957d09c4fd617e1101995c2b751dbb7
ewwgit <support@expertwebworx.com>
2016-12-27 15:29:56 +0530

    commited for change assets path

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -20,48 +20,30 @@ class AppAsset extends AssetBundle
     		'css/style.css',
     		'css/colors/green.css'
     ];
-    public $scripts = [
-    		
-    		'scripts/bootstrap.min.js',
-    		'scripts/chosen.jquery.min.js',
-    		'scripts/custom.js',
-    		'scripts/headroom.min.js',
-    		'scripts/jquery-2.1.3.min.js',
-    		'scripts/jquery.counterup.min.js',
-    		'scripts/jquery.flexslider-min.js',
-    		'scripts/jquery.gmaps.min.js',
-    		'scripts/jpanelmenu.js',
-    		'scripts/jquery.magnific-popup.min.js',
-    		'scripts/jquery.sceditor.bbcode.min.js',
-    		'scripts/jquery.sceditor.js',
-    		'scripts/jquery.superfish.js',
-    		'scripts/jquery.themepunch.revolution.min.js',
-    		'scripts/jquery.themepunch.showbizpro.min.js',
-    		'scripts/jquery.themepunch.tools.min.js',
-    		'scripts/stacktable.js',
-    		'scripts/switcher.js',
-    		'scripts/vendor-date.js',
-    		'scripts/vendor-datepicker.js',
-    		'scripts/Waypoints.min.js',
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
-    		
+    public $js = [
+    		//'js/jquery.js',
+    		'js/jquery-ui.js',
+    		'js/bootstrap.min.js',
+    		'js/chosen.jquery.min.js',
+    		'js/custom.js',
+    		'js/headroom.min.js',
+    		//'js/jquery-2.1.3.min.js',
+    		'js/jquery.counterup.min.js',
+    		'js/jquery.flexslider-min.js',
+    		'js/jquery.gmaps.min.js',
+    		'js/jquery.jpanelmenu.js',
+    		'js/jquery.magnific-popup.min.js',
+    		'js/jquery.sceditor.bbcode.min.js',
+    		'js/jquery.sceditor.js',
+    		'js/jquery.superfish.js',
+    		'js/jquery.themepunch.revolution.min.js',
+    		'js/jquery.themepunch.showbizpro.min.js',
+    		'js/jquery.themepunch.tools.min.js',
+    		'js/stacktable.js',
+    		'js/switcher.js',
+    		'js/vendor-date.js',
+    		'js/vendor-datepicker.js',
+    		'js/Waypoints.min.js',
     		
     ];
     public $depends = [
