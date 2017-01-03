212327bc1a7d668a25bebb13d5b498c8919b0f87
ewwgit <support@expertwebworx.com>
2017-01-03 12:44:15 +0530

    commited for remove extara login

--- frontend/modules/employercompany/views/empcommon/view.php
+++ frontend/modules/employercompany/views/empcommon/view.php
@@ -1,9 +1,6 @@
 <?php
-use kartik\social\Module;
 use kartik\social\FacebookPlugin;
 use yii\helpers\Url;
-$social = Yii::$app->getModule('social');
-$callback = Url::toRoute(['/employercompany/empcommon/validate-fb'], true); // or any absolute url you want to redirect
-echo $social->getFbLoginLink($callback, ['class'=>'btn btn-primary']);
+
 
 echo FacebookPlugin::widget(['type'=>FacebookPlugin::SHARE, 'settings' => []]);
