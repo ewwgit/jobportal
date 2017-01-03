e2d60efec0ade4260f1450d89b63ae2b9cb589cc
komali <support@EWW22.eww.local>
2017-01-03 18:53:31 +0530

    commited the social buttons in jobdetails view

--- frontend/modules/employercompany/views/empcommon/jobpostingsview.php
+++ frontend/modules/employercompany/views/empcommon/jobpostingsview.php
@@ -115,6 +115,23 @@ $this->title = 'Postings View';
 						</div>
 					</div>
 				</div>
+				
+				 
+ <div class="container">
+<div class="row">
+<div class="col-md-4">
+<p class="form-row form-row-wide">Social Share buttons :</p>
+<?= \bl\socialShare\widgets\SocialShareWidget::widget([
+        'componentId' => 'socialShare',
+        'url' => Url::toRoute(['empcommon/facebook'], true),
+        'title' => 'Job Portal',
+        'description' => 'Job seekers can browse through job openings posted by employers and apply for positions through the Job Portal....',
+        'image' => Url::toRoute(['frontend/web/images/logo.png'], true)
+    ]) ?>
+</div>
+</div>
+</div>
+				
 				<div class="clearfix"></div>
 			<div class="divider margin-top-0 padding-reset"></div>
           <?= Html::a('Back', ['/employercompany/empcommon/jobpostingslist'], ['class'=>'btn btn-primary button border fw margin-top-10'])?>
--- frontend/views/site/jobdetails.php
+++ frontend/views/site/jobdetails.php
@@ -31,6 +31,26 @@ use frontend\models\EmployeeJobapplied;
  
  </div>
  
+ <div class="container">
+<div class="row">
+<div class="col-md-4">
+<p class="form-row form-row-wide">Social Share buttons :</p>
+<?= \bl\socialShare\widgets\SocialShareWidget::widget([
+        'componentId' => 'socialShare',
+        'url' => Url::toRoute(['empcommon/facebook'], true),
+        'title' => 'Job Portal',
+        'description' => 'Job seekers can browse through job openings posted by employers and apply for positions through the Job Portal....',
+        'image' => Url::toRoute(['frontend/web/images/logo.png'], true)
+    ]) ?>
+</div>
+</div>
+</div>
+    
+ 
+ 
+ 
+ 
+ 
  
  
  <table class="table table-user-information ">
