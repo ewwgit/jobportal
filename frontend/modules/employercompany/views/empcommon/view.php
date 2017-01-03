2796a8457caec7dd86d3491401635a516f33b62c
santosh <santosh@EWW18.eww.local>
2017-01-03 18:38:06 +0530

    Udpated for social share url

--- frontend/modules/employercompany/views/empcommon/view.php
+++ frontend/modules/employercompany/views/empcommon/view.php
@@ -8,8 +8,8 @@ echo FacebookPlugin::widget(['type'=>FacebookPlugin::SHARE, 'settings' => []]);
 
 <?= \bl\socialShare\widgets\SocialShareWidget::widget([
         'componentId' => 'socialShare',
-        'url' => Url::toRoute(['site/index'], true),
-        'title' => 'Black Lamp - digital agancy',
-        'description' => 'Black Lamp provides a comprehensive range of services for development...',
-        'image' => Url::toRoute(['/logo.png'], true)
+        'url' => Url::toRoute(['empcommon/facebook'], true),
+        'title' => 'Job Portal',
+        'description' => 'Job seekers can browse through job openings posted by employers and apply for positions through the Job Portal....',
+        'image' => Url::toRoute(['frontend/web/images/logo.png'], true)
     ]) ?>
