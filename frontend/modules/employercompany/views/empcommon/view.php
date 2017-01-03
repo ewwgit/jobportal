9b92553735dea62541e9d4a9209ce44bab989dbd
santosh <santosh@EWW18.eww.local>
2017-01-03 18:44:04 +0530

    Updated social share page

--- frontend/modules/employercompany/views/empcommon/view.php
+++ frontend/modules/employercompany/views/empcommon/view.php
@@ -2,10 +2,14 @@
 use kartik\social\FacebookPlugin;
 use yii\helpers\Url;
 
-
-echo FacebookPlugin::widget(['type'=>FacebookPlugin::SHARE, 'settings' => []]);
+$this->title = 'Social Share';
+$this->params ['breadcrumbs'] [] = $this->title;
+//echo FacebookPlugin::widget(['type'=>FacebookPlugin::SHARE, 'settings' => []]);
 ?>
-
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<p class="form-row form-row-wide">Social Share buttons :</p>
 <?= \bl\socialShare\widgets\SocialShareWidget::widget([
         'componentId' => 'socialShare',
         'url' => Url::toRoute(['empcommon/facebook'], true),
@@ -13,3 +17,7 @@ echo FacebookPlugin::widget(['type'=>FacebookPlugin::SHARE, 'settings' => []]);
         'description' => 'Job seekers can browse through job openings posted by employers and apply for positions through the Job Portal....',
         'image' => Url::toRoute(['frontend/web/images/logo.png'], true)
     ]) ?>
+</div>
+</div>
+</div>
+    
