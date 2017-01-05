0fea104f54fe8b512d15f2e82129d67e2a4ffb5a
ewwgit <support@expertwebworx.com>
2017-01-05 13:03:48 +0530

    commited for employee id issue in home page

--- frontend/views/site/jobsview.php
+++ frontend/views/site/jobsview.php
@@ -14,7 +14,7 @@ use frontend\models\EmployeeJobapplied;
 	<td class="centered"><?= $model['city'];?></td>
 	<td class="title"><?= date('Y-m-d',strtotime( $model['createdDate']))?></td>
 	<?php
-	$userId = \Yii::$app->user->id;
+	$userId = Yii::$app->emplyoee->emplyoeeid;
 	$memberJoin = EmployeeJobapplied::getUsersjoined ( $model->id, $userId );
 	?>
 	<?php if($memberJoin ==0){?>
