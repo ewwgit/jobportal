c749deedd63d9bb577f7cffafab2d54879c537c1
santosh <santosh@EWW18.eww.local>
2017-01-19 12:49:27 +0530

    Updated with latest alignments

--- frontend/views/site/categoriesview.php
+++ frontend/views/site/categoriesview.php
@@ -5,5 +5,13 @@ use yii\helpers\Url;
 use yii\helpers\BaseStringHelper;
 $searchUrl = Yii::$app->urlManager->createUrl(['employees?EmployeeJobsearch%5Brolecategory%5D='.$model['role_name']]);
 ?>
-
+<style>
+.list-view {
+	min-height: 300px;
+}
+</style>
+<div class="row">
+<div class="col-md-3">
 <li> <a href="<?= $searchUrl; ?>"> <?=$model['role_name'] ?> Jobs</a></li>
+</div>
+</div>
--- frontend/views/site/locationsview.php
+++ frontend/views/site/locationsview.php
@@ -5,5 +5,13 @@ use yii\helpers\Url;
 use yii\helpers\BaseStringHelper;
 $searchUrl = Yii::$app->urlManager->createUrl(['employees?EmployeeJobsearch%5Bjob_location%5D%5B%5D='.$model['location']]);
 ?>
-
+<style>
+.list-view {
+	min-height: 300px;
+}
+</style>
+<div class="row">
+<div class="col-md-3">
 <li> <a href="<?= $searchUrl; ?>">Jobs in <?=$model['location'] ?></a></li>
+</div>
+</div>
